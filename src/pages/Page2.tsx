20802baee115ac33e8a203caedb55823208c05aa
yututi <y.tsuchiya0207@gmail.com>
2021-07-04 20:48:12 +0900

    init

---
+++ src/pages/Page2.tsx
@@ -0,0 +1,10 @@
+import { Link } from "gatsby"
+
+export default function Page2() {
+    return (
+        <>
+            <div>Page2</div>
+            <Link to="/">ホームに戻る</Link>
+        </>
+    )
+}
